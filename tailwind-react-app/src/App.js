11f776b3d5a56f689fe6f8a2aa3d5a28bdc66df3
harsh-trivedi09 <trivedihharsh@gmail.com>
2024-05-09 00:32:44 +0530

    task1 complete

--- tailwind-react-app/src/App.js
+++ tailwind-react-app/src/App.js
@@ -1,8 +1,24 @@
+import Group from "./Components/Group";
+import Navbar from "./Components/NavBar";
+import React, { useState } from 'react';
+
+
 export default function App() {
+  let [data,setdata]=useState(0)
+
+  function addCount(){
+    setdata(data+1)
+  }
+  function lessCount(){
+    if (data > 0) {
+      setdata(data -1);
+  }
+  }
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <Navbar addCount={addCount} lessCount={lessCount} data={data}/>
+      <Group addCount={addCount} lessCount={lessCount} data={data}/>
     </>
   );
 }
