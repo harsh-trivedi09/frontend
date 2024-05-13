b5c3a7de9cb47130dc007cce62526bc20ebc92a9
harsh-trivedi09 <trivedihharsh@gmail.com>
2024-05-13 19:38:51 +0530

    update

--- react/router-react/src/Component/Home.jsx
+++ react/router-react/src/Component/Home.jsx
@@ -1,9 +1,13 @@
-
+import { useNavigate } from "react-router-dom";
+import About from "./About";
 
 function Home() {
+    const navigate= useNavigate()
     return (  
         <>
         <h1>THis is Home page</h1>
+        <button onClick={()=>navigate('./About')}>About us</button>
+        <button onClick={()=>navigate('./Home')}>home us</button>
         </>
     );
 }
